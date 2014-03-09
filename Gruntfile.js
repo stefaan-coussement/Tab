464cb59716722ad78dc86755a85f658d15cf4431
Stefaan Coussement <stefaan.coussement@advalvas.be>
2014-03-09 17:20:32 +0000

    added grunt to build, test and livereload js

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,35 +1,76 @@
+/* globals module: true */
+
 module.exports = function(grunt) {
+    "use strict";
+    //jshint quotmark: true
 
     // Project configuration.
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
+        version: '<%= pkg.version %>'.split('+')[0],
         concat: {
-            options: {
-                banner: '/* <%= pkg.name %>.js - version <%= pkg.version %>, <%= grunt.template.today("yyyy-mmm-dd HH:MM") %>, license <%= pkg.license %>, copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.author %> */\n'
+            license: {
+                options: {
+                    banner: '/*\n',
+                    footer: '*/\n'
+                },
+                src: 'LICENSE',
+                dest: '_license.js'
+            },
+            test: {
+                src: [ '<%= concat.license.dest %>', 'src/<%= pkg.name %>-core.js', 'src/<%= pkg.name %>-callbacks.js' ],
+                dest: 'src/test/<%= pkg.name %>.js'
             },
             tab: {
-                src: 'src/<%= pkg.name %>.js',
+                src: [ '<%= concat.test.dest %>' ],
                 dest: '<%= pkg.name %>.js'
+            }
+        },
+        jshint: {
+            options: {
+                jshintrc: true
             },
-        }, 
+            test: [ '<%= concat.test.dest %>', 'src/test/qunit/*.js' ]
+        },
+        qunit: [ 'src/test/test-qunit.html' ],
         uglify: {
             options: {
-                banner: '/* <%= pkg.name %>.js - version <%= pkg.version %>, <%= grunt.template.today("yyyy-mmm-dd HH:MM") %>, license <%= pkg.license %>, copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.author %> */\n',
-                report: "gzip"
+                banner: '/* <%= pkg.name %>.js - version <%= version %>, <%= grunt.template.today("yyyy-mmm-dd HH:MM") %>, license <%= pkg.license %>, copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.author %> */\n',
+                report: 'gzip'
             },
-            tab_min: {
-                src: 'src/<%= pkg.name %>.js',
+            tab: {
+                src: '<%= pkg.name %>.js',
                 dest: '<%= pkg.name %>.min.js'
             }
-        }
+        },
+
+        watch: {
+            test: {
+                files: [ 'src/<%= pkg.name %>-*.js' ],
+                tasks: [ 'concat:license', 'concat:test', 'clean' ]
+            },
+            reload: {
+                options: {
+                    livereload: true
+                },
+                files: [ 'src/test/test-qunit-blanket.html', 'src/test/<%= pkg.name %>.js', 'src/test/qunit/*.js' ],
+                tasks: [ 'clean' ]
+            }
+        },
+
+        clean: [ '<%= concat.license.dest %>' ]
     });
 
+    grunt.loadNpmTasks('grunt-contrib-clean');
     grunt.loadNpmTasks('grunt-contrib-concat');
     grunt.loadNpmTasks('grunt-contrib-jshint');
     grunt.loadNpmTasks('grunt-contrib-qunit');
     grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-contrib-watch');
 
-    // Default tasks.
-    grunt.registerTask('default', ['concat', 'uglify']);
+    // Tasks.
+    grunt.registerTask('default', [ 'watch' ]);
+    grunt.registerTask('_', [ 'watch' ]);
+
+    grunt.registerTask('build', [ 'concat:license', 'concat:test', 'jshint', 'qunit', 'concat:tab', 'uglify', 'clean' ]);
 };
--- tab.js
+++ tab.js
@@ -1,24 +1,25 @@
-/* tab.js - version 1.0.0-alpha.1+1, 2014-Mar-09 11:39, license MIT, copyright (c) 2014 Stefaan Coussement */
-// The MIT License (MIT)
-//
-// Copyright (c) 2014 Stefaan Coussement
-//
-// Permission is hereby granted, free of charge, to any person obtaining a copy of
-// this software and associated documentation files (the "Software"), to deal in
-// the Software without restriction, including without limitation the rights to
-// use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
-// the Software, and to permit persons to whom the Software is furnished to do so,
-// subject to the following conditions:
-//
-// The above copyright notice and this permission notice shall be included in all
-// copies or substantial portions of the Software.
-// 
-// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
-// FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
-// COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
-// IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
-// CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+/*
+The MIT License (MIT)
+
+Copyright (c) 2014 Stefaan Coussement
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of
+this software and associated documentation files (the "Software"), to deal in
+the Software without restriction, including without limitation the rights to
+use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+the Software, and to permit persons to whom the Software is furnished to do so,
+subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
 
 /*global window: true, Tab: true */
 
@@ -865,6 +866,8 @@ Tab = (function (global) {
     return Tab;
 
 }(window));
+
+/*global Tab: true */
 
 //#################################################################################################
 //# CALLBACKS
